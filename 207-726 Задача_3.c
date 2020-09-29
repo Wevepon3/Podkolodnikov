758a68d0d67d51d494e4e7ce4d0a2fec6e143b86
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-09-29 11:20:45 +0300

    Add files via upload

---
+++ 207-726 Задача_3.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+main()
+{
+	float a, b, c;
+	printf("c = (a + b)/2");
+	printf("\n a - ");
+	scanf_s("%f", &a);
+	printf("\n b - ");
+	scanf_s("%f", &b);
+	c = (a + b) / 2;
+	printf("%.2f", c);
+}

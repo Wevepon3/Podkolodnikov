ac8a2d8f99233157037c0156be25e0dc6120bb39
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-06 20:14:49 +0300

    Add files via upload

--- 201-726 Задача_4.c
+++ 201-726 Задача_4.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include <math.h>
 main()
 {
-	float a, b, G, H, J, K;
-	printf("\n a - ");
+	float a, b, S, F, G, J;
+	printf("a - ");
 	scanf_s("%f", &a);
+	a = abs(a);
 	printf("\n b - ");
 	scanf_s("%f", &b);
-	printf("G = a + b");
-	G = a + b;
-	printf("\n G - %.2f", G);
-	printf("\n H = a * b");
-	H = a * b;
-	printf("\n H - %.2f", H);
+	b = abs(b);
+
+	printf("\n S = a + b");
+	S = a + b;
+	printf("\n %.2f S -", S);
+
 	if (a > b)
 	{
-		printf("\n J = a - b");
-		J = a - b;
-		printf("\n G - %.2f", J);
-		printf("\n K = (a * a) / (b * b)");
-		K = (a * a) / (b * b);
-		printf("\n G - %.2f", K);
+    printf("\n G = a - b");
+	G = a - b;
+	printf("\n %.2f G -", G);
+
+	printf("\n J = a / b");
+	J = a / b;
+	printf("\n %.2f J -", J);
+
+	printf("\n F = a * b");
+	F = a * b;
+	printf("\n %.2f F -", F);
 	}
 	else
 	{
-		printf("\n J = b - a");
-		J = b - a;
-		printf("\n G - %.2f", J);
-		printf("\n K = (b * b) / (a * a)");
-		K = (b * b) / (a * a);
-		printf("\n G - %.2f", K);
-	}
+		printf("\n G = b - a");
+		G = b - a;
+		printf("\n %.2f G -", G);
 
+		printf("\n J = b / a");
+		J = b / a;
+		printf("\n %.2f J -", J);
+
+		printf("\n F = a * b");
+		F = a * b;
+		printf("\n %.2f F -", F);
+	}
+	
+
+	
 }

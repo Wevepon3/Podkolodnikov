581e438fffb114fcb3da17a1bb931d5aae11a7d2
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-16 00:26:34 +0300

    Add files via upload

---
+++ Laba5.2 201-726.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+	float A, B, C;
+	printf("D = AB + BC");
+	printf("\n A - ");
+	scanf_s("%f", &A);
+	printf("\n B - ");
+	scanf_s("%f", &B);
+	printf("\n C - ");
+	scanf_s("%f", &C);
+	A = C - A;
+	B - C - B;
+	C = A + B;
+	printf("\n %.2f AC = ", A);
+	printf("\n %.2f BC = ", B);
+	printf("\n %.2f D = ", C);
+}

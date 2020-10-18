4a319c15583103e0f2450a59ede77db0a3308e03
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-18 16:57:50 +0300

    Add files via upload

---
+++ Laba6. 6 201-726.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	float A, x;
+	printf("A^8 = x");
+	printf("\n A - ");
+	scanf_s("%f", &A);
+	x = pow(A, 2);
+	A = pow(A, 2) * pow(A, 2);
+	x = x * x;
+	A = A * x;
+	printf("\n %.2f x = ", A);
+}

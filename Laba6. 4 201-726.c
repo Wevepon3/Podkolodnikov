d7936d059e99ab8c9751e63898c4815b5d789e02
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-18 16:32:37 +0300

    Add files via upload

---
+++ Laba6. 4 201-726.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	float y, x;
+	printf("y = 3x^6 - 6x^2 - 7 ");
+	printf("\n x - ");
+	scanf_s("%f", &x);
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	printf("\n %.2f y = ", y);
+}
+
+
+
+

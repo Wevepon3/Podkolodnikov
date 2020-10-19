3d3be295d9e5614950e2c4a4524c8564ee34cbbd
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-19 22:55:44 +0300

    Add files via upload

---
+++ Laba10. 2 201-726.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	float A, B, C;
+	printf("A - ");
+	scanf_s("%f", &A);
+	printf("B - ");
+	scanf_s("%f", &B);
+	printf("C - ");
+	scanf_s("%f", &C);
+	while (A < B && B < C)
+	{
+		printf("Istino");
+		return 0;
+	}
+	printf("Ne istino");
+}
+

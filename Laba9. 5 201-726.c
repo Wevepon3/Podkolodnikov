eb7898f1159469bd753780b04f33d4c6243a8821
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-19 22:34:38 +0300

    Add files via upload

---
+++ Laba9. 5 201-726.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int x, y, z;
+	printf("x - ");
+	scanf_s("%i", &x);
+	y = x / 100;
+	y = ceil(y);
+	z = x - y * 100;
+	y = y + 1;
+	printf("\n stoletie - %i \n god - %i", y, z);
+}
+

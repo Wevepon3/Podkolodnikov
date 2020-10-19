f49304fa73ba94acf60f8ad3d6ee6018ff3b8e46
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-19 23:01:03 +0300

    Add files via upload

---
+++ Laba10. 3 201-726.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	float A, B, C;
+	printf("A - ");
+	scanf_s("%f", &A);
+	while (9 < A && A < 100)
+	{
+		printf("Istino");
+		return 0;
+	}
+	printf("Ne istino");
+}
+

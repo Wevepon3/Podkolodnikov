9f2e3aa34fe529c9e8df1f8c4165761f73502ae1
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-19 22:03:56 +0300

    Add files via upload

---
+++ Laba9. 3 201-726.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int x, k;
+	printf("k - ");
+	scanf_s("%i", &x);
+	k = 0;
+	while (x > 6)
+	{
+		x = x - 7;
+		k = k + 1;
+
+	}
+	printf("\n %i x - ", x);
+	printf("\n %i k - ", k);
+}
+

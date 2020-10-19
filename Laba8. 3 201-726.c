3cda7333df34df4c94f4a1f5963864c1e2bcb50f
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-19 17:25:49 +0300

    Add files via upload

---
+++ Laba8. 3 201-726.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	float A, B, x, c;
+	printf("A - ");
+	scanf_s("%f", &A);
+	printf("\n B - ");
+	scanf_s("%f", &B);
+     x = A / B;
+	 x = floor(x);
+	 c = A - B * x;
+	printf("\n %.2f C - ", floor(c));
+}

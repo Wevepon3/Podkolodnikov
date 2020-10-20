09af8d7033d0ed848cef4f58f7577a025316f749
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 16:16:54 +0300

    Add files via upload

---
+++ Laba7. 3 201-726.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<math.h>
+main()
+{
+    int X, A, Y, k, k2;
+    float r;
+    printf("Izvestno   ");
+    scanf_s("%d", &X);
+    printf("Kg stoit konhpet ");
+    scanf_s("%d", &A);
+    printf("rubley\n");
+    k = A / X;
+    printf("Nayti stoimost dla  ");
+    scanf_s("%d", &Y);
+    printf("кг\n");
+    k2 = k * Y;
+    printf("1 kg konphet stoit %d\n", k);
+    printf("%d kg konphet stoit %d\n", Y, k2);
+
+}

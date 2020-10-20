f70d81fa87cdb6cbff9305fd57d70aacdb99f641
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 21:47:25 +0300

    Add files via upload

---
+++ Laba11. 2 201-726.c
@@ -0,0 +1,29 @@
+﻿
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int A, B, C, s;
+    printf(" A = ");
+    scanf_s("%d", &A);
+    printf(" B = ");
+    scanf_s("%d", &B);
+    printf(" C = ");
+    scanf_s("%d", &C);
+    if (A > B > C)
+    {
+        s = A + B;
+        printf("x = %d\n", s);
+    }
+    if ((A > B) && (B < C))
+    {
+        s = A + C;
+        printf("x = %d\n", s);
+    }
+    if ((A < B) && (B > C))
+    {
+        s = B + C;
+        printf("x = %d\n", s);
+    }
+    return 0;
+}

5cad7286f02d1313bf78985e4943ab8b5df6ef1e
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 16:23:46 +0300

    Add files via upload

---
+++ Laba7. 5 201-726.c
@@ -0,0 +1,16 @@
+﻿
+#include<stdio.h>
+#include<math.h>
+ main()
+{
+    int A, B;
+    float x;
+    printf("A*x + B = 0\n");
+    printf("koef A = ");
+    scanf_s("%d", &A);
+    printf("koef B = ");
+    scanf_s("%d", &B);
+    printf("%d*x + %d = 0\n", A, B);
+    x = -B / A;
+    printf("x = %.2f", x);
+}

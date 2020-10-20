f9fb68c3460620c88794ba2627b745c1e41c5c29
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 16:20:05 +0300

    Add files via upload

---
+++ Laba7. 4 201-726.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<math.h>
+ main()
+{
+    int V1, V2, S, S1, S2, T, K;
+    float r;
+    printf("V1 = ");
+    scanf_s("%d", &V1);
+    printf("V2 = ");
+    scanf_s("%d", &V2);
+    printf("S = ");
+    scanf_s("%d", &S);
+    printf("t  = ");
+    scanf_s("%d", &T);
+    S1 = V1 * T;
+    S2 = V2 * T;
+    K = S + S1 + S2;
+    printf("S %d ch  ravno %d km \n", T, K);
+
+}

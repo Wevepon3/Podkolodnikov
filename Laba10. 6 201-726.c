a2d6dc96cee19abb20e02086cb238ff9c3d5bafe
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 15:39:11 +0300

    Add files via upload

---
+++ Laba10. 6 201-726.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int A, B, C;
+	printf("A - ");
+	scanf_s("%i", &A);
+	printf("\n B - ");
+	scanf_s("%i", &B);
+	printf("\n C - ");
+	scanf_s("%i", &C);
+	if ((A * A + B * B == C * C) || (B * B + C * C == A * A) || (C * C + A * A == B * B))
+	{
+		printf("Pramougolnuy");
+		return 0;
+	}
+}

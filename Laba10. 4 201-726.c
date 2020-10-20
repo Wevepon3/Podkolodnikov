1b80267a7d79cff8f17afdd65e1ed55439788eb1
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 15:27:01 +0300

    Add files via upload

---
+++ Laba10. 4 201-726.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int A, x, y, z;
+	printf("A - ");
+	scanf_s("%i", &A);
+	x = A / 100;
+	y = A % 100 / 10;
+	z = A % 10;
+	if ((x > y) & (y > z))
+	{
+		printf("Ubuvaet");
+	}
+	if ((x < y) & (y < z))
+	{
+		printf("Vozrastaet");
+	}
+}
+
+

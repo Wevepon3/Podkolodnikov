71684a7afcff5b12cf48fcfecfa4a432c3fa597a
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 16:00:34 +0300

    Add files via upload

---
+++ Laba7. 1 201-726.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+#include<math.h>
+ main()
+{
+    int a;
+    float pi, r;
+    pi = 3.14;
+    printf("Ugol a, 0<a<360\n a = ");
+    scanf_s("%i", &a);
+    r = pi * a / 180;
+    printf("Ugol v radianah = %.2f\n", r);
+ }

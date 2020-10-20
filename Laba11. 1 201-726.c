6c5c0330b881d44acde8f6e5dda50ccfe84b300a
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 21:30:09 +0300

    Add files via upload

---
+++ Laba11. 1 201-726.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int A, B, x;
+	printf("A - ");
+	scanf_s("%i", &A);
+	printf("\n B - ");
+	scanf_s("%i", &B);
+	while (A != B)
+	{
+		if (A > B)
+		{
+			B = A;
+			printf("A - %i , B - %i", A, B);
+			return 0;
+		}
+		else
+		{
+			A = B;
+			printf("A - %i , B - %i", A, B);
+			return 0;
+		}
+	}
+	A = 0;
+	B = 0;
+	printf("A - %i , B - %i", A, B);
+}

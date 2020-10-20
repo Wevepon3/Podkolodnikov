b71facb0bcdd3d98243fae9b66bfe3be438f5ff8
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 15:28:59 +0300

    Add files via upload

---
+++ Laba10. 5 201-726.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int A, x, y, z, g;
+	printf("A - ");
+	scanf_s("%d", &A);
+	x = A / 1000;
+	y = A % 100 / 10;
+	z = A / 100 % 10;
+	g = A % 10;
+	while ((x = g) % (y = z))
+	{
+		printf("Chislo odinakovo sleva na pravo");
+		return 0;
+	}
+}

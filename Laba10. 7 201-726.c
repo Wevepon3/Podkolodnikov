fa63d3ee647b29618a60950946d64d4a965623ac
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-10-20 15:46:14 +0300

    Add files via upload

---
+++ Laba10. 7 201-726.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int A, B, C;
+	printf("A - ");
+	scanf_s("%i", &A);
+	printf("\n B - ");
+	scanf_s("%i", &B);
+	printf("\n C - ");
+	scanf_s("%i", &C);
+	while ((A<B+C) && (B<A+C) && (C<A+B))
+	{
+		printf("Suheystvuet");
+		return 0;
+	}
+}

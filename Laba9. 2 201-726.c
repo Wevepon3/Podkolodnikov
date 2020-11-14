bc572a567825151fdde1abb46ed09c98d7b4c421
Wevepon3 <71543817+Wevepon3@users.noreply.github.com>
2020-11-14 21:53:51 +0300

    Update Laba9. 2 201-726.c

--- Laba9. 2 201-726.c
+++ Laba9. 2 201-726.c
@@ -1,14 +1,42 @@
-﻿#include <stdio.h>
-#include <math.h>
-main()
+#include<stdio.h>
+#include<math.h>
+#include <locale.h>
+int main()
 {
-	int x, k;
-	printf("k - ");
-	scanf_s("%i", &x);
-	k = 0;
-	while (x > 6)
-	{
-		x = x - 7;
-	}
-	printf("\n %i x - ", x);
+    setlocale(LC_ALL, "Rus");
+    int K, c, n;
+    printf("K = ");
+    scanf_s("%d", &K);
+    c = K / 7;
+    c = floor(c);
+    n = K - 7 * c;
+    if (n == 0)
+    {
+        printf("День недели 0 - воскресенье");
+    }
+    if (n == 1)
+    {
+        printf("День недели 1 - понедельник");
+    }
+    if (n == 2)
+    {
+        printf("День недели 2 - вторник");
+    }
+    if (n == 3)
+    {
+        printf("День недели 3 - среда");
+    }
+    if (n == 4)
+    {
+        printf("День недели 4 - четверг");
+    }
+    if (n == 5)
+    {
+        printf("День недели 5 - пятница");
+    }
+    if (n == 6)
+    {
+        printf("День недели 6 - суббота");
+    }
+    return 0;
 }
